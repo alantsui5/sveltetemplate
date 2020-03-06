f66e14778f7c38399756ad95d47b62ab14f314f3
Your Name] <you@example.com>
2020-03-06 12:10:56 +0800

    Init with firebase

---
+++ src/pages/example/index.svelte
@@ -0,0 +1,20 @@
+<!-- routify:option name="example-app" -->
+
+<div style="width: 100%; text-align: center; margin-top: 4rem;">
+  <img src="/example/Routify-t.png" alt="" />
+  <div>
+    <b>Guide:</b>
+    <br />
+    <a href="https://routify.now.sh">https://routify.now.sh</a>
+  </div>
+  
+    <br />
+  <div>
+    <b>This template:</b>
+    <br />
+    <a href="https://github.com/sveltech/routify-starter">
+      https://github.com/sveltech/routify-starter
+    </a>
+  </div>
+
+</div>
---
+++ src/pages/example/modal/animated/[key].svelte
@@ -0,0 +1,15 @@
+<script>
+  import { goto, params } from "@sveltech/routify";
+  export let key, scoped;
+  const { send, receive, fade, activeKey } = scoped;
+  $: $activeKey = $params.key;
+</script>
+
+<div class="container" on:click={() => $goto('../')} transition:fade|local>
+  <div
+    class="modal"
+    in:receive|local={{ key: 'modal' }}
+    out:send|local={{ key: 'modal' }}>
+    {key}
+  </div>
+</div>
